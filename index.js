6ac47413623c3c7867f1bf82bfef2d71cfeedb83
claytonNishitaniHart <claytonnishitanihart@gmail.com>
2021-03-25 23:23:36 +1300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 const cookie_parser = require('cookie-parser');
 const { getUsers, postNewUser, findUserByEmail } = require('./db-access-layer');
 
-app.use(cors({ origin: process.env.ORIGIN }));
+// app.use(cors());
 app.use(express.json());
 app.use(cookie_parser());
 
